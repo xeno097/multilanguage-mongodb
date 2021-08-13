6f143e6e22eb15017f2ad492350a0e1291718921
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-13 15:08:13 -0400

    implemented car repository

--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { CarService } from './car.service';
 import { CarResolver } from './car.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CarEntity, CarEntitySchema } from './database/car.entity';
+import { CarRepository } from './car.repository';
 
 @Module({
-  providers: [CarResolver, CarService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: CarEntity.name,
+        schema: CarEntitySchema,
+      },
+    ]),
+  ],
+  providers: [
+    CarResolver,
+    CarService,
+    {
+      provide: CarRepository.name,
+      useClass: CarRepository,
+    },
+  ],
 })
 export class CarModule {}
---
+++ src/car/car.repository.ts
@@ -0,0 +1,143 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { LanguageCode } from 'src/common/enum/language-code.enum';
+import { IRepository } from 'src/common/interfaces/repository.interface';
+import { IRequestOptions } from 'src/common/interfaces/request-options.interface';
+import { IUpdateEntityDto } from 'src/common/interfaces/update-entity-dto.interface';
+import { CarEntity } from './database/car.entity';
+import { getCarAggregationPipeline } from './database/get-car-projection';
+import { CarDto } from './dto/car.dto';
+import { CreateCarDto } from './dto/create-car.dto';
+
+export class CarRepository implements IRepository<CarDto> {
+  constructor(
+    @InjectModel(CarEntity.name)
+    private readonly carEntityModel: Model<CarEntity>,
+  ) {}
+  private async _getOneEntity(
+    getOneEntityDto: Record<string, any>,
+    requestOptions: IRequestOptions,
+  ): Promise<CarEntity> {
+    try {
+      const res = await this.carEntityModel.findOne(getOneEntityDto);
+
+      if (!res) {
+        throw new Error('Not Found');
+      }
+
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async _getOneEntityByAggregationPipeline(
+    getOneEntityDto: Record<string, any>,
+    requestOptions: IRequestOptions = { language: LanguageCode.EN },
+  ) {
+    try {
+      const { language } = requestOptions;
+      const pipeline = [
+        {
+          $match: getOneEntityDto,
+        },
+        ...getCarAggregationPipeline(language),
+      ];
+
+      const [res] = await this.carEntityModel.aggregate(pipeline);
+
+      if (!res) {
+        throw new Error('Not Found');
+      }
+
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async getOneEntity(
+    getOneEntityDto: Record<string, any>,
+    requestOptions: IRequestOptions,
+  ): Promise<CarDto> {
+    const res = await this._getOneEntityByAggregationPipeline(
+      getOneEntityDto,
+      requestOptions,
+    );
+
+    return res;
+  }
+
+  public async getAllEntities(
+    requestOptions: IRequestOptions,
+  ): Promise<CarDto[]> {
+    const { language } = requestOptions;
+
+    const pipeline = getCarAggregationPipeline(language);
+
+    const res = await this.carEntityModel.aggregate(pipeline);
+
+    return res;
+  }
+
+  public async createEntity(createEntityDto: CreateCarDto): Promise<CarDto> {
+    try {
+      const entity = new this.carEntityModel(createEntityDto);
+
+      await entity.save();
+
+      const getOneEntityDto = { id: entity.id };
+
+      const car = await this._getOneEntityByAggregationPipeline(
+        getOneEntityDto,
+      );
+
+      return car;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async updateEntity(
+    updateEntityDto: IUpdateEntityDto,
+    requestOptions: IRequestOptions,
+  ): Promise<CarDto> {
+    try {
+      const { data, where } = updateEntityDto;
+      const entity = await this._getOneEntity(where, requestOptions);
+
+      entity.set(data);
+
+      await entity.save();
+
+      const car = await this._getOneEntityByAggregationPipeline(
+        where,
+        requestOptions,
+      );
+
+      return car;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async deleteEntity(
+    getOneEntityDto: Record<string, any>,
+    requestOptions: IRequestOptions,
+  ): Promise<CarDto> {
+    try {
+      const car = await this._getOneEntityByAggregationPipeline(
+        getOneEntityDto,
+        requestOptions,
+      );
+
+      const entity = await this._getOneEntity(getOneEntityDto, requestOptions);
+
+      await entity.remove();
+
+      return car;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
